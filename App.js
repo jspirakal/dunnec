4b12b23c9ea6a2432f5598b1a6123c1547bdad3d
Christopher Dunne <christopherdunne@ChristophersMBP.lan>
2019-03-27 17:47:02 +0000

    First commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import FirebaseLogin from "./FirebaseLogin";
+import firebase from 'firebase'
+
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAvPpzlPYk7dNB-ijJvpghJjsyVsm5AETg",
+    authDomain: "entertainmentrepublic-f0c4e.firebaseapp.com",
+    databaseURL: "https://entertainmentrepublic-f0c4e.firebaseio.com",
+    projectId: "entertainmentrepublic-f0c4e",
+    storageBucket: "entertainmentrepublic-f0c4e.appspot.com",
+    messagingSenderId: "436326701914"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <FirebaseLogin login={user => console.warn(user)}/>
+    )
+  }
+}
+
+export default App;
---
+++ config/Firebase.js
@@ -0,0 +1,11 @@
+import * as Firebase from 'firebase'
+
+var config = {
+    apiKey: "<AIzaSyAvPpzlPYk7dNB-ijJvpghJjsyVsm5AETg>",
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://entertainmentrepublic-f0c4e.firebaseio.com/",
+    projectId: "<entertainmentrepublic-f0c4e>",
+    
+  };
+
+  firebase.initializeApp(config);
